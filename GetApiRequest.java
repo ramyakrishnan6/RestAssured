74f93f93adb5bf78a0146219bc1bc0acb6682cbf
RamyaMD <50456292+ramyakrishnan6@users.noreply.github.com>
2025-07-10 15:36:42 +0530

    GET API
    In RestAssured, a GET API call is used to retrieve data from a server. It sends a request to a specified URI and expects a response (usually in JSON or XML format), without modifying any data on the server.

---
+++ GetApiRequest.java
@@ -0,0 +1,27 @@
+package restful;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Comget {
+
+	@Test
+	public void GetApiRequest() {
+
+		Response res = RestAssured.get("https://reqres.in/api/users?page=2");
+		System.out.println(res.asPrettyString());
+		System.out.println(res.getStatusCode());
+		System.out.println(res.getTime());
+		System.out.println(res.getBody());
+		System.out.println(res.getTime());
+		System.out.println(res.getHeader("Content-Type"));
+
+		int actualcode = res.getStatusCode();
+
+		int expectedcode = 200;
+		Assert.assertEquals(expectedcode, actualcode);
+	}
+}
